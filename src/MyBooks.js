dd642a9bccba6eab8c933810088dc38434d1273b
Pedro Morissawa <pmorissawa@gmail.com>
2019-02-19 13:22:41 -0300

    added state Shelf

--- src/MyBooks.js
+++ src/MyBooks.js
@@ -1,7 +1,18 @@
 import React, {Component} from 'react'
 import './App.css'
 
-class Book extends Component {
+const Book = (props) => {
+    return(
+        <div className="book">
+            <img src={props.cover} className="book-cover" alt={`${props.title} cover`}/>
+            <p className="book-title">{props.title}</p>
+            <p className="book-authors">{props.authors}</p>
+            <button className="book-shelf-changer"></button>
+        </div> 
+    )
+}
+
+class Shelf extends Component {
     state = {
         books: [
         {
@@ -10,9 +21,9 @@ class Book extends Component {
                 "William E. Shotts, Jr."
             ],
             imageLinks: {
-                smallThumbnail: "http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
-                thumbnail: "http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-            }
+                "thumbnail": "http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+            },
+            status: 'read'
         },
         {
             title: "Learning Web Development with React and Bootstrap",
@@ -24,25 +35,20 @@ class Book extends Component {
                 "smallThumbnail": "http://books.google.com/books/content?id=sJf1vQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
                 "thumbnail": "http://books.google.com/books/content?id=sJf1vQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
             },
-        }
+            status: 'want'
+        },
         ]
-    }
+    };
 
+    
     render(){
-        let books = this.state.books
-
+        const books = this.state.books
         return(
             books.map((book) => {
                 return(
-                <div className="book">
-                    <img src={book.imageLinks.thumbnail} className="book-cover" alt={book.title}/>
-                    <p className="book-title">{book.title}</p>
-                    <p className="book-authors">{book.authors}</p>
-                    <button className="book-shelf-changer"></button>
-                </div>
+                    <Book cover={book.imageLinks.thumbnail} title={book.title} authors={book.authors} />
                 )
-            })
-            
+            }) 
         )
     }
 }
@@ -50,8 +56,15 @@ class Book extends Component {
 class Container extends Component {
 render(){
     return(
-        <div className='shelf-one'>
-            <Book />
+        <div className='app'>
+            <div className='list-books-title'>
+                <h1>MyBooks App</h1>
+            </div>
+            <div className='list-books-content'>
+                <div className='bookshelf'>
+                    <Shelf />
+                </div>
+            </div>
         </div>
     )
 }
