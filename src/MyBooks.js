96a984fce60e6addb222e84ba0c465fe691935a0
Pedro Morissawa <pmorissawa@gmail.com>
2019-02-21 13:35:51 -0300

    added book search page and functionality

--- src/MyBooks.js
+++ src/MyBooks.js
@@ -1,96 +1,163 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
 import './App.css'
+import {Route} from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 
 const Book = (props) => {
     return(
-        <div className="book">
-            <img src={props.cover} className="book-cover" alt={`${props.title} cover`}/>
-            <p className="book-title">{props.title}</p>
-            <p className="book-authors">{props.authors}</p>
-            <button className="book-shelf-changer"></button>
-        </div> 
+        <li key={props.id}>
+            <div className='book'>
+                <div className='book-top'>
+                    <img src={props.cover} className="book-cover" alt={`${props.title} cover`}/>
+                    <div className="book-shelf-changer"> 
+                        <select>
+                            <option value='' disabled>Move to...</option>
+                            <option value='currentlyReading'>Currently Reading</option>
+                            <option value='wantToRead'>Want to Read</option>
+                            <option value='read'>Read</option>
+                            <option value='none'>None</option>
+                        </select>
+                    </div>
+                </div>
+                <p className="book-title">{props.title}</p>
+                <p className="book-authors">{props.authors}</p>
+            </div>
+        </li>
     )
 }
 
-const Shelf = (props) => {  
-    return(
-        <Book cover={props.cover} title={props.title} authors={props.authors} status={props.status} key={props.title}/>
-    )
-}
+class Search extends Component {
+    state = {
+        searchQuery: '',
+        results: [],
+    }
 
+    constructor(props) {
+        super(props)
+
+        this.setQuery = this.setQuery.bind(this)
+    }
+
+    setQuery(e) {
+        if(e) {
+            this.setState({searchQuery: e.target.value})
+            BooksAPI.search(e.target.value)
+            .then((results) => {
+                const bookSearch = results.map((bookResult) => {
+                    return bookResult
+                })
+                this.setState({results: bookSearch})
+                console.log(results)
+            })
+            .catch((err) => {
+                this.setState({results: []})
+            })
+        } else {
+            this.setState({results: []})
+        }
+        
+    }
+
+    render() {
+        const results = this.state.results
+
+        return (
+            <div>
+                <div className="search-books-bar">
+                    <a href='/'><button className="close-search"></button></a>
+                    <input type="text" placeholder="Search by title or author" onChange={this.setQuery} value={this.state.searchQuery}></input>
+                </div>
+                <div className="search-books-results">
+                    <ul className="books-grid">
+                        {results.map((book) => {
+                           return <Book cover={book.imageLinks ? book.imageLinks.thumbnail : 'http://i.imgur.com/sJ3CT4V.gif'} title={book.title} authors={book.authors} shelf={book.shelf} id={book.id} />
+                        })}
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
 
 class Container extends Component {
     state = {
-        books: [
-        {
-            title: "The Linux Command Line",
-            authors: [
-                "William E. Shotts, Jr."
-            ],
-            imageLinks: {
-                "thumbnail": "http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-            },
-            status: 'read'
-        },
-        {
-            title: "Learning Web Development with React and Bootstrap",
-            authors: [
-                "Harmeet Singh",
-                "Mehul Bhatt"
-            ],
-            imageLinks: {
-                "smallThumbnail": "http://books.google.com/books/content?id=sJf1vQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
-                "thumbnail": "http://books.google.com/books/content?id=sJf1vQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-            },
-            status: 'want'
-        },
-        ]
+        books: [],
     };
-//Dá pra fazer a lógica das prateleiras usando .filter e procurando pelo status certo. 
-//Três são geradas por padrão, o state fica dentro do Container e as funções com .filter definem o que vai pra cada.
+    
+    componentDidMount() {
+        BooksAPI.getAll()
+        .then((books) => {
+            this.setState(() => ({books}))
+        })
+    }
 
-    render(){
+    render() {
         const books = this.state.books
+
         return(
             <div className='app'>
-                <div className='list-books-title'>
-                    <h1>MyBooks App</h1>
-                </div>
-                <div className='list-books-content'>
-                    <div className='bookshelf'>
-                        {books.map((book) => {
-                            if(book.status === 'want'){
-                                return(
-                                    <Book cover={book.imageLinks.thumbnail} title={book.title} authors={book.authors} status={book.status} key={book.title}/>
-                                )
-                            } else {
-                                return(null)
-                            }
-                        })}
+                <Route exact path='/' render={() => (
+                    <div>
+                        <div className='list-books-title'>
+                            <h1>MyBooks App</h1>
+                        </div>
+                        <div className='list-books-content'>
+                        <div className='bookshelf'>
+                            <h2 className='bookshelf-title'>Currently Reading</h2>
+                            <ul className='books-grid'>
+                                {books.map((book) => {
+                                    if(book.shelf === 'currentlyReading'){
+                                        return(
+                                            <Book cover={book.imageLinks ? book.imageLinks.thumbnail : 'http://i.imgur.com/sJ3CT4V.gif'} title={book.title} authors={book.authors} shelf={book.shelf} id={book.id} />                                   
+                                        )
+                                    } else {
+                                        return(null)
+                                    }
+                                })}
+                            </ul>
+                        </div>
+                            <div className='bookshelf'>
+                                <h2 className='bookshelf-title'>Want To Read</h2>
+                                <div className='bookshelf-books'>
+                                <ul className='books-grid'>
+                                    {books.map((book) => {
+                                        if(book.shelf === 'wantToRead'){
+                                            return(
+                                                <Book cover={book.imageLinks ? book.imageLinks.thumbnail : 'http://i.imgur.com/sJ3CT4V.gif'} title={book.title} authors={book.authors} shelf={book.shelf} id={book.id} />
+                                            )
+                                        } else {
+                                            return(null)
+                                        }
+                                    })}
+                                </ul>
+                                </div>
+                            </div>
+                            <div className='bookshelf'>
+                                <h2 className='bookshelf-title'>Read</h2>
+                                <div className='bookshelf-books'>
+                                    <ul className='books-grid'>
+                                        {books.map((book) => {
+                                            if(book.shelf === 'read'){
+                                                return(                                           
+                                                    <Book cover={book.imageLinks ? book.imageLinks.thumbnail : 'http://i.imgur.com/sJ3CT4V.gif'} title={book.title} authors={book.authors} shelf={book.shelf} id={book.id} />                                            
+                                                )
+                                            } else {
+                                                return(null)
+                                            }
+                                        })}
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="/search" className="open-search"><button></button></a>
                     </div>
-                    <div className='bookshelf'>
-                        {books.map((book) => {
-                            if(book.status === 'read'){
-                                return(
-                                    <Book cover={book.imageLinks.thumbnail} title={book.title} authors={book.authors} status={book.status} key={book.title}/>
-                                )
-                            } else {
-                                return(null)
-                            }
-                        })}
-                    </div>
-                    <div className='bookshelf'>
-                        {books.map((book) => {
-                            if(book.status === 'have'){
-                                return(
-                                    <Book cover={book.imageLinks.thumbnail} title={book.title} authors={book.authors} status={book.status} key={book.title}/>
-                                )
-                            } else {
-                                return(null)
-                            }
-                        })}
-                    </div>
-                </div>
+                )}/>
+
+                <Route path='/search' render={({history}) => ( 
+                    <Fragment>
+                        <Search/>   
+                    </Fragment>
+                )} />
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Container from './MyBooks'
+import {BrowserRouter} from 'react-router-dom'
 import './index.css'
 
-ReactDOM.render(<Container />, document.getElementById('root'))
+ReactDOM.render(
+<BrowserRouter>
+    <Container />
+</BrowserRouter>, document.getElementById('root'))
