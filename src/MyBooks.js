a1559aaf318203a26c3dbb6d1ec2ded2516ef764
Pedro Morissawa <pmorissawa@gmail.com>
2019-02-22 20:06:07 -0300

    fix book/shelf logic added shelfing for books

--- src/MyBooks.js
+++ src/MyBooks.js
@@ -3,27 +3,54 @@ import './App.css'
 import {Route} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 
-const Book = (props) => {
-    return(
-        <li key={props.id}>
-            <div className='book'>
-                <div className='book-top'>
-                    <img src={props.cover} className="book-cover" alt={`${props.title} cover`}/>
-                    <div className="book-shelf-changer"> 
-                        <select>
-                            <option value='' disabled>Move to...</option>
-                            <option value='currentlyReading'>Currently Reading</option>
-                            <option value='wantToRead'>Want to Read</option>
-                            <option value='read'>Read</option>
-                            <option value='none'>None</option>
-                        </select>
-                    </div>
+class Bookshelf extends Component {
+    
+    constructor(props){
+        super(props)
+        this.updateShelf = this.updateShelf.bind(this)
+    }
+    
+    updateShelf(e, book) {
+        BooksAPI.update(book, e.target.value)
+        .then(this.props.onUpdate())
+    }
+
+    render(){
+        const {books, title} = this.props
+
+        return(
+            <div className="bookshelf">
+                {title && (   
+                    <h2 className="bookshelf-title">{title}</h2>
+                )}
+                <div className="bookshelf-books">
+                    <ul className="books-grid">
+                        {books.map((book) => (
+                            <li key={book.id}>
+                                <div className="book">
+                                    <div className="book-top">
+                                        <img src={book.imageLinks ? book.imageLinks.smallThumbnail : "http://i.imgur.com/sJ3CT4V.gif"} className="book-cover" alt={`${book.title} cover`}/>
+                                        <div className="book-shelf-changer"> 
+                                            <select onChange={(e) => this.updateShelf(e, book)}
+                                            value={typeof book.shelf === "undefined" ? "none" : book.shelf}>
+                                                <option value="" disabled>Move to...</option>
+                                                <option value="currentlyReading">Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <p className="book-title">{book.title}</p>
+                                    <p className="book-authors">{book.authors ? book.authors[0] : "Unknown Author"}</p>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
-                <p className="book-title">{props.title}</p>
-                <p className="book-authors">{props.authors}</p>
             </div>
-        </li>
-    )
+        )
+    }
 }
 
 class Search extends Component {
@@ -44,10 +71,15 @@ class Search extends Component {
             BooksAPI.search(e.target.value)
             .then((results) => {
                 const bookSearch = results.map((bookResult) => {
-                    return bookResult
-                })
+                    this.props.books.map((book) => {
+                        if (book.id === bookResult.id) {
+                            bookResult.shelf = book.shelf
+                        }
+                        return bookResult
+                   })
+                   return bookResult
+               })
                 this.setState({results: bookSearch})
-                console.log(results)
             })
             .catch((err) => {
                 this.setState({results: []})
@@ -68,11 +100,7 @@ class Search extends Component {
                     <input type="text" placeholder="Search by title or author" onChange={this.setQuery} value={this.state.searchQuery}></input>
                 </div>
                 <div className="search-books-results">
-                    <ul className="books-grid">
-                        {results.map((book) => {
-                           return <Book cover={book.imageLinks ? book.imageLinks.thumbnail : 'http://i.imgur.com/sJ3CT4V.gif'} title={book.title} authors={book.authors} shelf={book.shelf} id={book.id} />
-                        })}
-                    </ul>
+                    <Bookshelf books={results} onUpdate={this.props.onUpdate}/>
                 </div>
             </div>
         )
@@ -83,8 +111,17 @@ class Container extends Component {
     state = {
         books: [],
     };
+
+    constructor(props) {
+        super(props)
+        this.getBooks = this.getBooks.bind(this)
+    }
     
     componentDidMount() {
+        this.getBooks()
+    }
+
+    getBooks = () => {
         BooksAPI.getAll()
         .then((books) => {
             this.setState(() => ({books}))
@@ -93,6 +130,9 @@ class Container extends Component {
 
     render() {
         const books = this.state.books
+        const currentlyReading = books.filter(cr => cr.shelf === "currentlyReading")
+        const wantToRead = books.filter(want => want.shelf === "wantToRead");
+        const read = books.filter(read => read.shelf === "read");
 
         return(
             <div className='app'>
@@ -102,60 +142,17 @@ class Container extends Component {
                             <h1>MyBooks App</h1>
                         </div>
                         <div className='list-books-content'>
-                        <div className='bookshelf'>
-                            <h2 className='bookshelf-title'>Currently Reading</h2>
-                            <ul className='books-grid'>
-                                {books.map((book) => {
-                                    if(book.shelf === 'currentlyReading'){
-                                        return(
-                                            <Book cover={book.imageLinks ? book.imageLinks.thumbnail : 'http://i.imgur.com/sJ3CT4V.gif'} title={book.title} authors={book.authors} shelf={book.shelf} id={book.id} />                                   
-                                        )
-                                    } else {
-                                        return(null)
-                                    }
-                                })}
-                            </ul>
-                        </div>
-                            <div className='bookshelf'>
-                                <h2 className='bookshelf-title'>Want To Read</h2>
-                                <div className='bookshelf-books'>
-                                <ul className='books-grid'>
-                                    {books.map((book) => {
-                                        if(book.shelf === 'wantToRead'){
-                                            return(
-                                                <Book cover={book.imageLinks ? book.imageLinks.thumbnail : 'http://i.imgur.com/sJ3CT4V.gif'} title={book.title} authors={book.authors} shelf={book.shelf} id={book.id} />
-                                            )
-                                        } else {
-                                            return(null)
-                                        }
-                                    })}
-                                </ul>
-                                </div>
-                            </div>
-                            <div className='bookshelf'>
-                                <h2 className='bookshelf-title'>Read</h2>
-                                <div className='bookshelf-books'>
-                                    <ul className='books-grid'>
-                                        {books.map((book) => {
-                                            if(book.shelf === 'read'){
-                                                return(                                           
-                                                    <Book cover={book.imageLinks ? book.imageLinks.thumbnail : 'http://i.imgur.com/sJ3CT4V.gif'} title={book.title} authors={book.authors} shelf={book.shelf} id={book.id} />                                            
-                                                )
-                                            } else {
-                                                return(null)
-                                            }
-                                        })}
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
+                            <Bookshelf books={currentlyReading} title="Currently Reading" onUpdate={this.getBooks}/>
+                            <Bookshelf books={wantToRead} title="Want to Read" onUpdate={this.getBooks}/>
+                            <Bookshelf books={read} title="Read" onUpdate={this.getBooks}/>
+                        </div>                        
                         <a href="/search" className="open-search"><button></button></a>
                     </div>
                 )}/>
 
-                <Route path='/search' render={({history}) => ( 
+                <Route path='/search' render={() => ( 
                     <Fragment>
-                        <Search/>   
+                        <Search books={books} onUpdate={this.getBooks}/>   
                     </Fragment>
                 )} />
             </div>
